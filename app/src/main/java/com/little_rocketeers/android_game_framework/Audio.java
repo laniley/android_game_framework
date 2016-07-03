5065ebaab73d95babedc61a2dca9bd186817d7e0
laniley <melanie.mende@me.com>
2016-07-03 19:51:38 +0200

    initial android game classes

--- app/src/main/java/com/little_rocketeers/android_game_framework/Audio.java
+++
@@ -1,9 +0,0 @@
-package com.little_rocketeers.android_game_framework;
-
-/**
- * Created by Melanie on 03.07.2016.
- */
-public interface Audio {
-    public Music createMusic(String file);
-    public Sound createSound(String file);
-}
--- app/src/main/java/com/little_rocketeers/android_game_framework/Sound.java
+++
@@ -1,9 +0,0 @@
-package com.little_rocketeers.android_game_framework;
-
-/**
- * Created by Melanie on 03.07.2016.
- */
-public interface Sound {
-    public void play(float volume);
-    public void dispose();
-}
